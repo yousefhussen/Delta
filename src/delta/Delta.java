13fd8cbe5bb7c662160262f71f8dd4ca9995f312
Mohamed Hussein <mohamed.hussein.1.9.9.71.9.9.8@gmail.com>
2020-12-15 05:47:33 +0200

    outer Layout set
    used the GridBagLayout to customize the panels as required (same as Grid Layout but givin weights to grids and adding some paddings and margins)

--- src/delta/Delta.java
+++ src/delta/Delta.java
@@ -7,7 +7,7 @@ static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     public static void main(String[] args) {
         
         JFrame appFrame = new JFrame("Hello");
-        GridBagLayout gridLayout = new GridBagLayout();
+        
         appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         appFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         JPanel panelNew = new JPanel();
@@ -18,11 +18,25 @@ static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         panelTest.setBackground(Color.yellow);
         panelReady.setBackground(Color.blue);
         panelMenu.setBackground(Color.BLACK);
-        //panelMenu.setSize(50, screenSize.height);
+        Container c = appFrame.getContentPane();
+        c.setLayout(new GridBagLayout());
+        GridBagConstraints gbc_for_container = new GridBagConstraints(); 
+        gbc_for_container.insets = new Insets(5,5,5,5);
+        gbc_for_container.gridx = 0;
+        gbc_for_container.gridy = 0;
+        gbc_for_container.ipadx = screenSize.width/10;
+        gbc_for_container.ipady = screenSize.height - screenSize.height/11;
+        c.add(panelMenu,gbc_for_container);
+        gbc_for_container.gridx = 1;
+        gbc_for_container.ipadx = (screenSize.width - (screenSize.width/8));
+        gbc_for_container.ipady = screenSize.height - screenSize.height/11;
+        c.add(panelNew,gbc_for_container);
+        FlowLayout menu_buttons_layout = new FlowLayout();
+        panelMenu.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         
-        appFrame.setLayout(gridLayout);
-        appFrame.add(panelMenu);
-       // appFrame.add(panelNew);
+        JButton newButton = new JButton("NEW");
+        panelMenu.setLayout(menu_buttons_layout);
+        panelMenu.add(newButton);
         
         
         
